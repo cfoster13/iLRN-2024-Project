ff3822a04848404e4c8f9af534ecedb0e8aa3ab7
cfoster13 <w1768986@my.westminster.ac.uk>
2024-05-28 18:25:07 +0100

    Resizing windows and adding scene manager script

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+
+    public void loadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if the next scene index is within the valid range
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Next scene index is out of range.");
+        }
+    }
+}
