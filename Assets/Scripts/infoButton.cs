fb11e31c5cf34883c310e64ca23d26c18cfd8410
cfoster13 <w1768986@my.westminster.ac.uk>
2024-06-08 00:04:34 +0100

    Added text for rows and columns
    Minor fixes as well

--- Assets/Scripts/infoButton.cs
+++ Assets/Scripts/infoButton.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class infoButton : MonoBehaviour
 {
     public Button helpButton;
     public Image helpInfo;
+    public GameObject rowPanel;
+    public GameObject colPanel;
     
 
     bool helpInfoDisplayed = false;
@@ -28,11 +31,15 @@ public class infoButton : MonoBehaviour
         {
             
             helpInfo.gameObject.SetActive(true);
+            rowPanel.SetActive(true);
+            colPanel.SetActive(true);
             helpInfoDisplayed = true;
         }
         else
         {
             helpInfo.gameObject.SetActive(false);
+            rowPanel.SetActive(false);
+            colPanel.SetActive(false);
             helpInfoDisplayed = false;
         }
 
